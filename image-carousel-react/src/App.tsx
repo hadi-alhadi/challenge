6de4420c76fd68dfff338a84b42adac22fc45fe5
hadi-alhadi <alhadi.h1993@gmail.com>
2024-03-29 18:35:27 +0400

    feat: Frontend - Add eslint

--- image-carousel-react/src/App.tsx
+++ image-carousel-react/src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import './App.css';
+import React from "react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./App.css";
 import Header from "./components/Header/header";
 import Carousel from "./components/Carousel/carousel";
+
 function App() {
-    return (
-        <div>
-            <Header />
-            <Carousel />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
--- image-carousel-react/src/hooks/useFetchDiscounts.ts
+++ image-carousel-react/src/hooks/useFetchDiscounts.ts
@@ -1,36 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export interface Discount {
-    id: number;
-    name: string;
-    image: string;
-    discount_percentage: number;
+  id: number;
+  name: string;
+  image: string;
+  discount_percentage: number;
 }
 
 const useFetchDiscounts = (name: string, discountPercentage: string) => {
-    const [data, setData] = useState<Discount[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState<Discount[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const response = await fetch(`${process.env.REACT_APP_API_URL}/discounts?name=${name}&discount_percentage=${discountPercentage}`);
-                const data = await response.json();
-                setData(data);
-            } catch (error) {
-                setError(error instanceof Error ? error.message : String(error));
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/discounts?name=${name}&discount_percentage=${discountPercentage}`,
+        );
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        setError(error instanceof Error ? error.message : String(error));
+      } finally {
+        setLoading(false);
+      }
+    };
 
-            } finally {
-                setLoading(false);
-            }
-        };
+    fetchData();
+  }, [name, discountPercentage]);
 
-        fetchData();
-    }, [name, discountPercentage]);
-
-    return { data, loading, error };
+  return { data, loading, error };
 };
 
 export default useFetchDiscounts;
